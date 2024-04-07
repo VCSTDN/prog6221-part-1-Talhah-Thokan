346d3999d454e4e7952948038442cbf1853fc440
talhahthokan_st10122437 <st10122437@vcconnect.edu.za>
2024-04-07 15:44:52 +0200

    Git - Recipe Class + Arrays to store data within user entries

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace RecipeApp
     {
         static void Main(string[] args)
         {
-            
+            Recipe recipe = new Recipe();   //init Recipe class
 
             // Prompt user to enter recipe details
             Console.WriteLine("Enter the details for the recipe:");
@@ -23,7 +23,7 @@ namespace RecipeApp
                 Console.Write("Unit of Measurement: ");
                 string unit = Console.ReadLine();
 
-                
+                recipe.AddIngredient(name, quantity, unit);
             }
 
             Console.Write("Number of steps: ");                             //Loop created to store muti data of number if steps to be used to capture in Array
@@ -38,9 +38,49 @@ namespace RecipeApp
             // Display the full recipe
             Console.WriteLine("\nFull Recipe:");                           //Display full recipe (Ingredients + Steps)                                     
 
-
         }
 
     }
 
+
+    class Recipe                                        //Class Recipe to Store and Call data of the user data
+    {
+        private string[] ingredients;
+        private string[] steps;
+
+        public Recipe()
+        {
+            ingredients = new string[0];                //Ingredients Array Created to store Ingredients
+            steps = new string[0];              //Number of Steps Array Created to store Ingredients
+        }
+
+        public void AddIngredient(string name, double quantity, string unit)                //Method created to store ingredients to Array Ingredients
+        {
+            Array.Resize(ref ingredients, ingredients.Length + 1);
+            ingredients[ingredients.Length - 1] = $"{quantity} {unit} of {name}";
+        }
+
+        public void AddStep(string step)                //Method created to store steps to Array steps
+        {
+            Array.Resize(ref steps, steps.Length + 1);
+            steps[steps.Length - 1] = step;
+        }
+
+        public void DisplayRecipe()                 //Method to display recipe
+        {
+            Console.WriteLine("Ingredients:");
+            foreach (var ingredient in ingredients)
+            {
+                Console.WriteLine(ingredient);          //Print each Ingredient
+            }
+
+            Console.WriteLine("\nSteps:");
+            for (int i = 0; i < steps.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {steps[i]}"); //Print each step
+            }
+        }
+
+
+    }
 }
