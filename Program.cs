23e38b07fd8a2ec1dbd2ba1278e5e51204467080
talhahthokan_st10122437 <st10122437@vcconnect.edu.za>
2024-04-07 15:31:56 +0200

    Git Initial - Enter Recipe Details, Number of Ingredients & Qty of Ingredients - Read & Write

--- Program.cs
+++ Program.cs
@@ -1,15 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ST10122437_PROG6221_POE_TalhahThokan
+namespace RecipeApp
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            
+
+            // Prompt user to enter recipe details
+            Console.WriteLine("Enter the details for the recipe:");
+            Console.Write("Number of ingredients: ");
+            int numIngredients = int.Parse(Console.ReadLine()); //Read & Write Data as per program instructions for end User
+
+            for (int i = 0; i < numIngredients; i++)                        //Loop created to store muti data of ingredients to be used to capture in Array
+            {
+                Console.WriteLine($"Enter details for ingredient {i + 1}:");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Quantity: ");
+                double quantity = double.Parse(Console.ReadLine());
+                Console.Write("Unit of Measurement: ");
+                string unit = Console.ReadLine();
+
+                
+            }
         }
+
     }
+
 }
